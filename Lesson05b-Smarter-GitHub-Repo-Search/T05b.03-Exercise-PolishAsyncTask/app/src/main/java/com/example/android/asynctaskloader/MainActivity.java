ec59b4a57844cbe199b81bf001d655c064fbcbaf
Stefanie Molin <stefaniemolin@gmail.com>
2018-03-11 12:54:19 -0400

    Finish exercise T05b.03

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.03-Exercise-PolishAsyncTask/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -165,7 +165,8 @@ public class MainActivity extends AppCompatActivity implements
     public Loader<String> onCreateLoader(int id, final Bundle args) {
         return new AsyncTaskLoader<String>(this) {
 
-            // TODO (1) Create a String member variable called mGithubJson that will store the raw JSON
+            // COMPLETED (1) Create a String member variable called mGithubJson that will store the raw JSON
+            String mGihubJson;
 
             @Override
             protected void onStartLoading() {
@@ -181,8 +182,13 @@ public class MainActivity extends AppCompatActivity implements
                  */
                 mLoadingIndicator.setVisibility(View.VISIBLE);
 
-                // TODO (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
-                forceLoad();
+                // COMPLETED (2) If mGithubJson is not null, deliver that result. Otherwise, force a load
+                if(mGihubJson != null){
+                    deliverResult(mGihubJson);
+                    mLoadingIndicator.setVisibility(View.INVISIBLE);
+                } else {
+                    forceLoad();
+                }
             }
 
             @Override
@@ -207,8 +213,13 @@ public class MainActivity extends AppCompatActivity implements
                 }
             }
 
-            // TODO (3) Override deliverResult and store the data in mGithubJson
-            // TODO (4) Call super.deliverResult after storing the data
+            // COMPLETED (3) Override deliverResult and store the data in mGithubJson
+            @Override
+            public void deliverResult(String data) {
+                // COMPLETED (4) Call super.deliverResult after storing the data
+                mGihubJson = data;
+                super.deliverResult(data);
+            }
         };
     }
 
